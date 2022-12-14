fb96869bf7c0ce1ec6745abc849874c1dd93a326
heikomaru <black145xd@gmail.com>
2022-12-13 21:35:14 -0400

    js empiezo

---
+++ js/hora.js
@@ -0,0 +1,8 @@
+function first() {
+    document.getElementById("lunes").style.display = "block";
+    document.getElementById("martes").style.display = "none";
+}
+function second() {
+    document.getElementById("lunes").style.display = "none";
+    document.getElementById("martes").style.display = "block";
+}
